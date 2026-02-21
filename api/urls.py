b9a3f367df7472b201bfb1480100a93c70013f98
haridinesh00 <haridinesh00@gmail.com>
2026-02-21 13:07:12 +0530

    url updated

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import YouTubeSearchAPIView, YouTubeDownloadAPIView
 
 urlpatterns = [
-    path('example/', views.example_view, name='example_view'),
+    # path('example/', views.example_view, name='example_view'),
     path('youtube-search/', YouTubeSearchAPIView.as_view(), name='youtube_search'),
     path('youtube-download/', YouTubeDownloadAPIView.as_view(), name='youtube_download'),
 ]
