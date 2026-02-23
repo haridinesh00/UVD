8dbf73120b2796fe7c329ca97d32dcdd61f58d36
haridinesh00 <haridinesh00@gmail.com>
2026-02-24 02:44:45 +0530

    Add database configuration using dj-database-url and update requirements

--- youtube_search_download/settings.py
+++ youtube_search_download/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,6 +82,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 TEMPLATES = [
     {
